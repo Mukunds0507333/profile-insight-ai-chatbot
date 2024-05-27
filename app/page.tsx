cbb8eb4121a2f2bdfc3c86bdce45f71f9af4694e
Mukund Sureshkumar <mukunds0507333@gmail.com>
2024-05-27 20:58:43 +0530

    mui

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Button from '@mui/material/Button';
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      
+       <Button variant="outlined">Outlined</Button>
     </main>
   );
 }
